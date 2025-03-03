4134fffefd2b6be571f97ac1dc3070127b225672
Prashanth <nagaprashanth@sutantra-software.com>
2025-03-03 14:52:46 +0530

    Add blog routes in betacode

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,12 +4,20 @@ const authController = require("../controllers/authController");
 const countriesController = require("../controllers/countriesController");
 const checkAuthenticate = require("../middlewares/checkAuthenticate");
 const countryformController = require("../controllers/countryformController");
+const blogController = require("../controllers/blogController");
 const uploadImages = require("../middlewares/uploadImages");
 
 router.post("/api/register", authController.register);
 router.post("/api/login", authController.login);
 router.get("/api/dashboard", checkAuthenticate, authController.getDashboard);
 router.post("/api/logout", checkAuthenticate, authController.logout);
+
+router.get("/api/getsingleblog/:id", blogController.getSingleblog);
+router.post("/api/addblog", blogController.createblog);
+router.get("/api/getallblogs", blogController.getAllBlogs);
+router.put("/api/updateblog/:id", blogController.updateblog);
+router.delete("/api/deleteblog/:id", blogController.deleteblog);
+router.post("/api/likeblog/:id", blogController.likeblog);
 
 router.post("/api/insertCountry", countriesController.insertCountry);
 router.put("/api/updateCountry/:id", countriesController.updateCountry);
